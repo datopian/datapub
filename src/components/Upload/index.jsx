e74f773f0a9cec153e1facdce6737c14a8d9ca9b
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-08-07 22:38:37 -0300

    [add][s] configured fetch to authenticate and get JWT token from authz - #6

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -19,13 +19,43 @@ class Upload extends React.Component {
       status: null,
     });
 
+    /**
+     * Push stuff
+     */
+    const config = {
+      authToken: 'be270cae-1c77-4853-b8c1-30b6cf5e9878',
+      api: 'http://localhost:5000',
+      organizationId: 'myorg',
+      datasetId: 'new-test-3',
+    }
+
+    const scopes = {
+      "scopes": [`obj:${config.organizationId}/${config.datasetId}/*:read,write`]
+    }
+
     // create an instance of a object
     const file = new FileAPI.HTML5File(event.target.files[0])
-    const uploader = new Uploader('key', 'organization-name', 'dataset-name', 'api')
+    const uploader = new Uploader(`${config.authToken}`, `${config.organizationId}`, `${config.datasetId}`, `${config.api}`)
 
-    console.log(event.target.files[0]);
-    console.log(uploader)
-    console.log(file)
+
+    // authz path
+    const path = '/api/action/authz_authorize'
+
+    // Get the JWT token from authz
+    fetch(`${path}`,{
+        method: 'POST',
+        headers: {  
+          'Content-Type':'application/json',
+          Authorization: config.authToken,
+        },
+        body: JSON.stringify(scopes)
+      })
+      .then(response => response.json())
+      .then(response => console.log(response))
+    
+    // console.log(event.target.files[0]);
+    // console.log(uploader)
+    // console.log(file)
   };
 
   onClickHandler = () => {

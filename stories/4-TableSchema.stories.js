1479e4a81d93e317dba475e98c9cfffc245f2bda
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-08-26 08:53:55 -0300

    [add][s]  add more types and change the table example - #13

--- stories/4-TableSchema.stories.js
+++ stories/4-TableSchema.stories.js
@@ -1,37 +1,50 @@
-
 import React from "react";
-import { action } from '@storybook/addon-actions';
+import { action } from "@storybook/addon-actions";
 
-import TableSchema from '../src/components/TableSchema'
+import TableSchema from "../src/components/TableSchema";
 
 export default {
-  title: 'Components /TableSchema',
+  title: "Components /TableSchema",
   component: TableSchema,
-}
+};
 
-const Template = (args) => (<TableSchema {...args} />);
+const Template = (args) => <TableSchema {...args} />;
 
 export const Default = Template.bind({});
 
 Default.args = {
-    schema: {
-        fields: [
-            {
-              title: '',
-              name: 'a',
-              type: 'string',
-              description: 'column a is about X',
-              format: '...'
-            },
-            {
-              title: '',
-              name: 'b',
-              type: 'integer',
-              description: 'column b is about X',
-              format: '...'
-            }
-          ]
-    },
-    data:[{ a: 1, b: 2}, {a: 5, b: 10}]
+  schema: {
+    fields: [
+      {
+        title: "",
+        name: "name",
+        type: "string",
+        description: "",
+        format: "",
+      },
+      {
+        title: "",
+        name: "age",
+        type: "integer",
+        description: "",
+        format: "",
+      },
+      {
+        title: "",
+        name: "address",
+        type: "string",
+        description: "",
+        format: "",
+      },
+    ],
+  },
+  data: [
+    { name: "Eathan Pritchard", age: 25, address: "1201 Tompkins Dr Madison" },
+    { name: "Zidan Berg", age: 22, address: "1309 Tompkins Dr Madison" },
+    { name: "Raisa Kinney", age: 32, address: "1497 Tompkins Dr Madison" },
+    { name: "Cara Woodley", age: 30, address: "1197  Buckeye Rd  Madison" },
+    { name: "Komal Robbins", age: 42, address: "1192  Buckeye Rd  Madison" },
+    { name: "Deacon Childs", age: 28, address: "1027 Tompkins Dr Madison" },
+    { name: "Ayse Shaw", age: 21, address: "1233 Buckeye Rd Madison" },
+  ],
 };
-

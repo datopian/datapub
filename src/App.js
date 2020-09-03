cdefb1d6e39186d02c94d78b5c32517e53fa7f55
anuveyatsu <anuar.ustayev@gmail.com>
2020-09-03 18:20:26 +0600

    [setup][xs]: convert it to class component so we can have class methods and clear state.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 import Upload from './components/Upload'
 
-export function ResourceEditor() {
-  return (
-    <div className="App">
-      <Upload/>
-    </div>
-  );
+export class ResourceEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Upload/>
+      </div>
+    );
+  }
 }
 
 export default ResourceEditor;

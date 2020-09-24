c6ddaecf80efb9ff9911e0e359050da3766ce6fd
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-09-24 15:47:55 -0300

    [remove hyphen form uiid][s] create remove hyphen function and add tests  - #37

--- src/utils/Utils.test.js
+++ src/utils/Utils.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { getFileExtension, onFormatTitle, onFormatName, onFormatBytes } from './index'
+import { getFileExtension, onFormatTitle, onFormatName, onFormatBytes, removeHyphen } from './index'
 
 describe("Utils", () => {
  
@@ -34,5 +34,12 @@ describe("Utils", () => {
     expect(onFormatName("sample_data.html")).toEqual('sample_data');
     expect(onFormatName("sample-data.xls")).toEqual('sample-data');
     expect(onFormatName("sample-Data.doc")).toEqual('sample-Data');
+  });
+
+  it('remove hyphen from uuid', () => {
+
+    expect(removeHyphen("fd77e419-32ae-4025-8d14-890343b605a3")).toEqual('fd77e41932ae40258d14890343b605a3');
+    expect(removeHyphen("9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d")).toEqual('9b1deb4d3b7d4bad9bdd2b0d7b3dcb6d');
+    expect(removeHyphen("should not broken without hyphen")).toEqual('should not broken without hyphen');
   });
 });

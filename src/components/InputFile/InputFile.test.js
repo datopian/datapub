16ba78d081ddc08d9dad114d475ee0bb992e6545
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-09-29 09:55:25 -0300

    [tests][s]: add tests for InputFile component - #37

---
+++ src/components/InputFile/InputFile.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+
+import InputFile from ".";
+
+describe("<InputFile />", () => {
+  const onChangeHandler = jest.fn(() => "sample");
+
+  it("render InputFile without crashing", () => {
+    const wrapper = shallow(<InputFile onChangeHandler={onChangeHandler} />);
+    expect(wrapper.contains("Drag and drop your files")).toEqual(true);
+  });
+
+  it("can select a file", () => {
+    const wrapper = shallow(<InputFile onChangeHandler={onChangeHandler} />);
+    const input = wrapper.find("input");
+
+    input.simulate("change", {
+      target: {
+        files: ["sample.csv"],
+      },
+    });
+
+    expect(onChangeHandler).toHaveBeenCalled();
+  });
+});

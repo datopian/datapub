447c45e422b056b88d3e0b8c8400c44c21ef67d3
Shahar Evron <shahar.evron@datopian.com>
2020-11-17 15:11:48 +0200

    Remove committed storybook-static dir

--- storybook-static/react-syntax-highlighter_languages_highlight_vbscriptHtml.a59bca9b78f05ed3b6dd.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[169],{2226:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.a59bca9b78f05ed3b6dd.bundle.js.map
